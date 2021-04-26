954fea28acfcae2959ba7c1613e57623ef8e8d3b
george.muresan <george.muresan@fortech.ro>
2021-04-26 16:21:23 +0300

    project initialization

---
+++ src/main/java/com/george/tracker/MacroCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.george.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/george/tracker/MacroCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacroCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
